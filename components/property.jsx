21240c3fb291eb595eb195c01e8ddefdb0bcf421
personnal <flowingkhaos@gmail.com>
2024-07-28 21:44:57 -0400

    removing image optimization from next to respect vercel account limits

--- components/property.jsx
+++ components/property.jsx
@@ -1,40 +1,77 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { Box, Flex, Text } from '@chakra-ui/react';
-import { Avatar } from '@chakra-ui/avatar';
-import { FaBed, FaBath } from 'react-icons/fa';
-import { BsGridFill } from 'react-icons/bs';
-import { GoVerified } from 'react-icons/go';
-import millify from 'millify';
+import Link from "next/link";
+import Image from "next/image";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { Avatar } from "@chakra-ui/avatar";
+import { FaBed, FaBath } from "react-icons/fa";
+import { BsGridFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
+import millify from "millify";
 
-import DefaultImage from '../assets/images/house.jpg';
+import DefaultImage from "../assets/images/house.jpg";
 
-const property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID  } }) => (
-    <Link href={`/property/${externalID}`} passHref>
-        <Flex flexWrap='wrap' w='420px' p='5' paddingTop='0px' justifyContent='flex-start' cursor='pointer' >
-        <Box>
-            <Image src={coverPhoto ? coverPhoto.url : DefaultImage} alt="housePhoto" width={400} height={260} />
-        </Box>
-        <Box w='full'>
-        <Flex paddingTop='2' alignItems='center' justifyContent='space-between'>
-          <Flex alignItems='center'>
-            <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified />}</Box>
-            <Text fontWeight='bold' fontSize='lg'>CAD {millify(price)}{rentFrequency && `/${rentFrequency}`}</Text>
+const property = ({
+  property: {
+    coverPhoto,
+    price,
+    rentFrequency,
+    rooms,
+    title,
+    baths,
+    area,
+    agency,
+    isVerified,
+    externalID,
+  },
+}) => (
+  <Link href={`/property/${externalID}`} passHref>
+    <Flex
+      flexWrap="wrap"
+      w="420px"
+      p="5"
+      paddingTop="0px"
+      justifyContent="flex-start"
+      cursor="pointer"
+    >
+      <Box>
+        <img
+          src={coverPhoto ? coverPhoto.url : DefaultImage}
+          alt="housePhoto"
+          width={400}
+          height={260}
+          loading="lazy"
+        />
+      </Box>
+      <Box w="full">
+        <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+          <Flex alignItems="center">
+            <Box paddingRight="3" color="green.400">
+              {isVerified && <GoVerified />}
+            </Box>
+            <Text fontWeight="bold" fontSize="lg">
+              CAD {millify(price)}
+              {rentFrequency && `/${rentFrequency}`}
+            </Text>
           </Flex>
           <Box>
-            <Avatar size='sm' src={agency?.logo?.url}></Avatar>
+            <Avatar size="sm" src={agency?.logo?.url}></Avatar>
           </Box>
         </Flex>
-        <Flex alignItems='center' p='1' justifyContent='space-between' w='250px' color='blue.400'>
+        <Flex
+          alignItems="center"
+          p="1"
+          justifyContent="space-between"
+          w="250px"
+          color="blue.400"
+        >
           {rooms}
           <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
         </Flex>
-        <Text fontSize='lg'>
-          {title.length > 30 ? title.substring(0, 30) + '...' : title}
+        <Text fontSize="lg">
+          {title.length > 30 ? title.substring(0, 30) + "..." : title}
         </Text>
-        </Box>
-        </Flex>
-    </Link>
-)
+      </Box>
+    </Flex>
+  </Link>
+);
 
-export default property
+export default property;
